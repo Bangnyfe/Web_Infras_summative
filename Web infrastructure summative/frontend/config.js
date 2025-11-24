2452a7753bea407da122f58f2c73a8bc598d12db
A-DAIB <a.daib@alustudent.com>
2025-11-24 23:33:35 +0200

    Delete Web infrastructure summative directory

--- Web infrastructure summative/frontend/config.js
+++
@@ -1,7 +0,0 @@
-// API Configuration Template
-// Copy this file to config.js and add your actual API key
-// DO NOT commit config.js to version control
-
-window.API_CONFIG = {
-    apiKey: '308645fb88fa577fec8c3b3a4ff2d324acdf283b2fd0f5d141a05d24f6bfd6ea'
-};
