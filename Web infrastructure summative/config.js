b0bbaa27d8efb9651e1e6f39e08fbbb9e436e0fb
Bangnyfe <a.daib@alustudent.com>
2025-11-24 11:30:51 +0200

    Adding the complete config file

---
+++ Web infrastructure summative/config.js
@@ -0,0 +1,7 @@
+// API Configuration Template
+// Copy this file to config.js and add your actual API key
+// DO NOT commit config.js to version control
+
+window.API_CONFIG = {
+    apiKey: 'f83a35a43b120283b3080f86857ba5fe8dfeb7781cfebb2f9525e398f0fd0545'
+};
